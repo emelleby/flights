a22602a027a4638f444628aa717e0066548b0019
emelleby <emelleby@gmail.com>
2024-11-17 23:33:55 +0100

    remove extra slash

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://flights-by-scope321.replit.app/api/v1/';
+const API_BASE_URL = 'https://flights-by-scope321.replit.app/api/v1';
 
 export interface FlightEmissions {
   economy: number;
